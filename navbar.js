4ecf5f77a199111547aa5e7a996a5077a50534f6
doulat <douladwd123@gmail.com>
2023-04-23 00:48:10 -0700

    DoulatmaharPortfolioUpdated

---
+++ navbar.js
@@ -0,0 +1,55 @@
+document.body.style.backgroundImage = "url('https://www.toptal.com/designers/subtlepatterns/uploads/hypnotize.png')";
+
+document.body.style.backgroundAttachment = "fixed";
+
+document.body.style.backgroundPosition = "center";
+
+
+let blogwebsiteadmin=document.getElementById("blogwebsiteadmin")
+let showBLoggwebsite=()=>{
+blogwebsiteadmin.style.display="flex"
+}
+let showBLoggwebsiteleave=()=>{
+    blogwebsiteadmin.style.display="none"
+    }
+
+
+    let excellenceAcademy=document.getElementById("excellenceAcademy")
+let excellenceaca=()=>{
+    excellenceAcademy.style.display="flex"
+}
+let excellenceacaleave=()=>{
+    excellenceAcademy.style.display="none"
+    }
+
+
+
+    let blogs=document.getElementById("blogs")
+let showBlogs=()=>{
+    blogs.style.display="flex"
+}
+let showBlogsleave=()=>{
+    blogs.style.display="none"
+    }
+
+
+    let moveUserAdmin=()=>{
+        window.location.assign("https://useradmin-reactassignment.netlify.app/")
+    }
+
+
+    
+    let moveExcellence=()=>{
+        window.location.assign("https://excellence-center.netlify.app/")
+    }
+
+
+    
+    let moveblogs=()=>{
+        window.location.assign("https://blogwebsite-reactassignment.netlify.app/")
+    }
+
+
+
+  
+      
